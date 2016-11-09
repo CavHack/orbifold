7b15df2b803d3a30d68abd0f25ff7501a8503614
Draco Cavalier <karl@blackesquirel.com>
2016-11-09 03:04:26 -0800

    import routes
    for some bloody reason this was wrong.

--- src/routes/dev.js
+++ src/routes/dev.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import Reach from 'react';
+import React from 'react';
 import Router from 'react-router';
+import routes from '/.react-routes';
 import proxy from 'express-http-proxy';
 import express from 'express';
 
